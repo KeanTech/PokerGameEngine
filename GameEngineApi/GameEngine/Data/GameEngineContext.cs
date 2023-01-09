3dcb530056b19b8ad679e7a51ea9fe6a77d30195
Alexandar Lackovic <92458374+Xandarlh02@users.noreply.github.com>
2023-01-09 12:38:15 +0100

    Classes and added context

---
+++ GameEngineApi/GameEngine/Data/GameEngineContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GameEngine.Models.Game;
+
+namespace GameEngine.Data
+{
+    public class GameEngineContext : DbContext
+    {
+        public GameEngineContext (DbContextOptions<GameEngineContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GameEngine.Models.Game.User> User { get; set; } = default!;
+    }
+}

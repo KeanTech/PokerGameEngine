52d01b86e25d68e408877a4abdae296503a59f27
Kean <kenethanders@gmail.com>
2023-01-09 12:43:13 +0100

    Added model GameState

---
+++ GameEngineApi/GameEngine/Models/Game/GameState.cs
@@ -0,0 +1,9 @@
+﻿namespace GameEngine.Models.Game
+{
+    public class GameState
+    {
+        public PokerTable PokerTable { get; set; }
+        public int PlayerId { get; set; }
+        public string PlayerIdentifier { get; set; }
+    }
+}

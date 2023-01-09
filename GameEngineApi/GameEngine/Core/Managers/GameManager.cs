1be5a600b522cd4d993544d116d64654a60f7abd
Kean <kenethanders@gmail.com>
2023-01-09 12:41:54 +0100

    Added GameManager with a constructor with private fields on Webhook and string. Also added method GetCurrentGame

--- GameEngineApi/GameEngine/Core/Managers/GameManager.cs
+++ GameEngineApi/GameEngine/Core/Managers/GameManager.cs
@@ -2,5 +2,25 @@
 {
     public class GameManager
     {
+        private readonly WebHook _webHook;
+        private readonly string _gameState;
+
+        public GameManager(WebHook webHook, string gameState) 
+        {
+            _webHook = webHook;
+            _gameState = gameState;
+        }
+
+        private void GetCurrentGame(GameState gameState) 
+        {
+            // User GameStateService to get the current game state
+            // this should get called when the gameController get call 
+
+            // Game stat is used to see the previous players turn and set the next players turn
+        }
+
+
+
+
     }
 }

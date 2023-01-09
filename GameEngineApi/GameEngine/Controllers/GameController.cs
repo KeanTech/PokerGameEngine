6e4c805b1cf7b96d35d0d0052c6e904a6b1d45fb
Kean <kenethanders@gmail.com>
2023-01-09 08:49:03 +0100

    Added player and subscription controller
    Added the endpoints with routes

--- GameEngineApi/GameEngine/Controllers/GameController.cs
+++ GameEngineApi/GameEngine/Controllers/GameController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameEngine.Core.Managers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameEngine.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class GameController : Controller
     {
-        public IActionResult Index()
+        private readonly GameManager _gameManager;
+
+        public GameController(GameManager gameManager) 
         {
-            return View();
+            _gameManager = gameManager;
         }
+
+        [HttpPost]
+        [Route("Call")]
+        public IActionResult Call() 
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Fold")]
+        public IActionResult Fold() 
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Raise")]
+        public IActionResult Raise() 
+        {
+            return Ok();   
+        }
+
+        [HttpPost]
+        [Route("Check")]
+        public IActionResult Check() 
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("AllIn")]
+        public IActionResult AllIn() 
+        {
+            return Ok();
+        }
+
     }
 }
---
+++ GameEngineApi/GameEngine/Controllers/SubscriptionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameEngine.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscriptionController : Controller
+    {
+        [HttpGet]
+        [Route("Subcribe")]
+        public IActionResult Subscrib()
+        {
+            return Ok();
+        }
+    }
+}

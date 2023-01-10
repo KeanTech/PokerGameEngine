cc55a800486b6b502db1f317b371ebb3b0b4aff4
Morten Vestergaard <mort286f@zbc.dk>
2023-01-10 14:31:26 +0100

    initial commit of CardManager

---
+++ GameEngineApi/GameEngine/Core/Managers/CardManager.cs
@@ -0,0 +1,37 @@
+﻿using GameEngine.Models.Game;
+
+namespace GameEngine.Core.Managers
+{
+    public class CardManager : ICardManager
+    {
+        private WebHook WebHook;
+        private GameManager GameManager;
+        public List<Player> GameWinner { get; set; }
+        public List<Card> PlayerCards { get; set; }
+        public List<Card> CardsOnBoard { get; set; }
+        public int HandValue { get; set; }
+
+        public CardManager(List<Card> playerCards, List<Card> cardsOnBoard)
+        {
+            PlayerCards = playerCards;
+            CardsOnBoard = cardsOnBoard;
+        }
+
+        public void ValidatePlayerHand(List<Card> playerHand, List<Card> cardsOnBoard)
+        {
+            List<Card> cardsToCheck = new List<Card>();
+            cardsToCheck.AddRange(playerHand);
+            cardsToCheck.AddRange(cardsOnBoard);
+        }
+
+        public void ValidateCardsInHand(List<Card> cards) 
+        {
+            
+        }
+
+        public void ValidateCardsOnBoard(List<Card> cards)
+        {
+
+        }
+    }
+}
---
+++ GameEngineApi/GameEngine/Core/Managers/ICardManager.cs
@@ -0,0 +1,9 @@
+﻿using GameEngine.Models.Game;
+
+namespace GameEngine.Core.Managers
+{
+    public interface ICardManager
+    {
+        void ValidatePlayerHand(List<Card> playerHand, List<Card> cardsOnBoard);
+    }
+}

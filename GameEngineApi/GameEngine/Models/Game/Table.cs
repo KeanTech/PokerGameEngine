0963c42086a9e2914b6da73e453b2e60794e6682
Alexandar Lackovic <92458374+Xandarlh02@users.noreply.github.com>
2023-01-11 09:08:46 +0100

    Update Table.cs

--- GameEngineApi/GameEngine/Models/Game/Table.cs
+++ GameEngineApi/GameEngine/Models/Game/Table.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public List<Card> Cards { get; set; }
-        public List<Card> CardDeck { get; set; }
+        public Stack<Card> CardDeck { get; set; }
 		public int Chips { get; set; }
         public List<Player> Players { get; set; }
 

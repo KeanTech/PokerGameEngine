fcce20f2e11946518bac2ebce407b905ce0be12d
Kean <kenethanders@gmail.com>
2023-01-11 12:15:35 +0100

    Added Player functions Call, Raise, AllIn, Fold and Check to IGameManager

--- GameEngineApi/GameEngine/Core/Managers/IGameManager.cs
+++ GameEngineApi/GameEngine/Core/Managers/IGameManager.cs
@@ -1,18 +1,26 @@
-﻿using GameEngine.Models.Game;
+﻿using GameEngine.Models.Events;
+using GameEngine.Models.Game;
+using System.Reflection;
 
 namespace GameEngine.Core.Managers
 {
     public interface IGameManager
     {
-        public GameState StartNewGame(int tabelId);
+        GameState StartNewGame(int tabelId);
         GameState ClearTable();
         void EndGame(GameState gameState);
         GameState GetCurrentGame(int tableId);
         GameState GetNewCardDeck(GameState gameState);
         GameState GiveCards(int amountOfCards, GameState gameState);
         GameState ResetPlayerBet();
-        void SetPlayerTurn();
+        void SetPlayerTurn(GameState gameState);
         GameState UpdateChipsPool();
+        void PlayerCall(BetEvent betEvent);
+        void PlayerRaise(BetEvent betEvent);
+        void PlayerAllIn(BetEvent betEvent);
+        void PlayerFold(int playerId, string userIdentifier);
+        void PlayerCheck(int playerId, string userIdentifier);
+
 
     }
 }

27a4a8832265f93d652724b93164ff0ae03eb9dc
Kean <kenethanders@gmail.com>
2023-01-14 14:56:15 +0100

    Added new manager to handle dao models

---
+++ GameEngineApi/GameEngine/Core/Managers/DataManager.cs
@@ -0,0 +1,40 @@
+﻿using GameEngine.Models.Game;
+
+namespace GameEngine.Core.Managers
+{
+    public class DataManager
+    {
+        public static List<DeckCard> GetDeckCards(List<Card> cards, GameState gameState)
+        {
+            List<DeckCard> deckCards = new List<DeckCard>();
+
+            foreach (var card in cards)
+            {
+                DeckCard deckCard = new DeckCard() 
+                { 
+                    Card = card,
+                    CardId = card.Id,
+                    PokerTable = gameState.PokerTable,
+                    TableId = gameState.PokerTable.Id
+                };
+                
+                deckCards.Add(deckCard);
+            }
+
+            return deckCards;
+        }
+
+        public static List<Card> GetCardsFromDeck(List<DeckCard> deckCards) 
+        {
+            List<Card> cards = new List<Card>();  
+
+            foreach (var deckCard in deckCards)
+            {
+                cards.Add(deckCard.Card);
+            }
+
+            return cards;   
+        }
+
+    }
+}
